2cda4e16a3882fbd4c2bfaca100194513445f281
Gustavo Britzke <gustavo@tropa.digital>
2020-10-06 00:02:11 -0300

    First

---
+++ public/index.php
@@ -0,0 +1,316 @@
+<?php include "partials/header.phtml" ?>
+<link href="./assets/css/index.css" media="screen" rel="stylesheet" type="text/css">
+
+<div id="banner" class="bg-blue-800">
+    <img src="assets/img/home/banner/bg-effect-1.svg" alt="Logo Contabilix" class="banner-effect banner-effect1">
+    <img src="assets/img/home/banner/bg-effect-2.svg" alt="Logo Contabilix" class="banner-effect banner-effect2">
+    <div class="content">
+        <div class="owl-carousel">
+            <div class="row">
+                <div class="col-lg-7 col-md-12 text-banner">
+                    <p class="cl-white font-xg f-weight-700 tiltle">
+                        Contabilidade online, fácil e
+                        segura para sua empresa
+                    </p>
+                    <p class="font-m cl-white list"><span class="bullet cl-yellow-100">•</span> Controle sua empresa através de uma <span class="cl-yellow-200 f-weight-700">platafoma totalmente digital.</span></p>
+                    <p class="font-m cl-white list"><span class="bullet cl-yellow-100">•</span> Contabilidade completa a partir de  <span class="cl-yellow-200 f-weight-700">R$ 98/mês.</span></p>
+                    <p class="font-m cl-white list"><span class="bullet cl-yellow-100">•</span><span class="cl-yellow-200 f-weight-700">Atendimento especializado</span> via Telefone e Whatsapp para todos os clientes.</p>
+                    <div class="buttons">
+                        <a href="javascript:;" class="bt bg-green-200 cl-white font-s bt-1">ABRIR MINHA EMPRESA <span class="bg-yellow-300 cl-blue-800 font-pp">grátis</span></a>
+                        <a href="javascript:;" class="bt bg-green-200 cl-white font-s bt-2">TROCAR DE CONTADOR <span class="bg-yellow-300 cl-blue-800 font-pp">1º mês grátis</span></a>
+                    </div>
+                    <span class="cl-white font-m f-weight-700 selo"><img src="assets/img/home/banner/icon-garantia.svg" alt="5 anos de experiência"> 5 ANOS DE EXPERIÊNCIA NO MERCADO!</span>
+                </div>
+                <div class="col-lg-5 col-md-12 foto-banner">
+                    <img src="assets/img/home/banner/image-1.svg">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="quem-somos">
+    <div class="content">
+        <div class="row">
+            <div class="col-12 title" align="center">
+                <h1 class="font-g cl-gray-300 f-weight-400">Somos uma Plataforma de <b>Contabilidade Online especializada em pequenas empresas.</b> </h1>
+                <p class="cl-gray-300 font-s">Nosso principal objetivo é acabar com a burocracia, criando uma relação moderna da sua empresa com o contador.</p>
+            </div>
+            <div class="col-3 pd-right" >
+                <img src="assets/img/home/quem-somos/crcsp.svg" alt="Logo conselho regional de contabilidade de São Paulo">
+                <img src="assets/img/home/quem-somos/crcsp.svg" alt="Logo conselho federal de contabilidade">
+                <p class="font-pp cl-gray-300"><a href="javascript:;" class="f-weight-700 cl-gray-300">Clique aqui</a> e consulte nosso registro do Conselho Federal de Contabilidade</p>
+            </div>
+            <div class="col-lg-9 col-md-12 pd-left">
+                <p class="cl-gray-300 font-s">Graças a automatização de todas as rotinas executadas nos escritórios tradicionais de contabilidade,  conseguimos reduzir drasticamente o custo do contador para as pequenas empresas.</p>
+                <p class="cl-gray-300 font-s">Importante ressaltar que aliamos a tecnologia aos nossos contadores, ou seja, apesar de nosso sistema dar conta de várias rotinas de sua empresa de forma automática, haverá sempre um contador disponível para você falar por telefone, e-mail ou Whatsapp quando precisar tirar uma dúvida ou de qualquer tipo de ajuda. </p>
+                <div class="row localizacoes">
+                    <div class="col-4">
+                        <img src="assets/img/home/quem-somos/regioes.svg" alt="Regiões atendidas">
+                    </div>
+                    <div class="col-8">
+                        <span class="cl-blue-800 font-m"><b>CIDADES ATENDIDAS: </b></span>
+                        <span class="cl-gray-300 font-s">São Paulo </span>
+                        <span class="cl-gray-300 font-s">Rio de Janeiro </span>
+                        <span class="cl-gray-300 font-s">Belo Horizonte. </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="video" class="bg-gray-100">
+    <div class="content">
+        <div class="row">
+            <div class="col-12">
+                <div class="bg-video bg-green-200">
+                    <iframe width="800" height="450" src="https://www.youtube.com/embed/GNDAqNHou5Q" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="ajuda">
+    <div class="content">
+        <div class="row">
+            <div class="col-12 title" align="center">
+                <h2 class="font-g cl-gray-300 f-weight-400"><b>Veja como a Contabilix pode lhe ajudar no dia a dia</b> </h2>
+                <p class="cl-gray-300 font-s">Seja para abrir uma empresa ou realizar o serviço contábil dela, aqui você encontra <span class="cl-blue-800 f-weight-700">praticidade</span> e <span class="cl-blue-800 f-weight-700">economia</span>.</p>
+            </div>
+            <div class="row boxes">
+                <div class="col-4">
+                    <span class="font-m cl-gray-300 f-weight-700"><img src="assets/img/home/ajuda/icon-1.svg" alt="Icone Abertura de Empresa Grátis">Abertura de Empresa Grátis</span>
+                    <p class="cl-gray-300 font-s">Para quem não tem tempo nem dinheiro a perder com a burocracia. O processo é gratuito e 100% online.</p>
+                </div>
+
+                <div class="col-4">
+                    <span class="font-m cl-gray-300 f-weight-700"><img src="assets/img/home/ajuda/icon-2.svg" alt="Icone Emissor Simplificado de Notas Fiscais ">Emissor Simplificado de Notas Fiscais </span>
+                    <p class="cl-gray-300 font-s">De forma fácil e prática, você emite uma nota fiscal para seu cliente pela própria plataforma da Contabilix. E claro, se você tiver qualquer dúvida, terá sermpre um de nossos especialistas em emissão de notas disponível para ajudar.</p>
+                </div>
+
+                <div class="col-4">
+                    <span class="font-m cl-gray-300 f-weight-700"><img src="assets/img/home/ajuda/icon-3.svg" alt="Icone Cálculo e Emissão das Guias de Impostos">Cálculo e Emissão das Guias de Impostos</span>
+                    <p class="cl-gray-300 font-s">Fazemos os cálculos e disponibilizamos as guias de impostos da sua empresa sempre no mínimo 5 dias antes do vencimento. Basta você baixar e realizar os pagamentos para manter sua empresa em dia.</p>
+                </div>
+
+                <div class="col-4">
+                    <span class="font-m cl-gray-300 f-weight-700"><img src="assets/img/home/ajuda/icon-4.svg" alt="Icone Calendário de Atividades">Calendário de Atividades</span>
+                    <p class="cl-gray-300 font-s">Fique por dentro das obrigações contábeis de sua empresa. Vamos informar sempre que uma ação sua for necessária, como pagamento de tributos, importação de notas e atualização de informações.</p>
+                </div>
+
+                <div class="col-4">
+                    <span class="font-m cl-gray-300 f-weight-700"><img src="assets/img/home/ajuda/icon-5.svg" alt="Icone Folha de Pagamento de Funcionários e Pró-Labore">Folha de Pagamento de Funcionários e Pró-Labore</span>
+                    <p class="cl-gray-300 font-s">Realizamos a emissão do(s) contra-cheque(s), da(s) guia(s) de imposto(s) e todas as obrigações acessórias referente a área trabalhista. É só cadastrar na plataforma e realizar os pagamentos, que fazemos todo o resto.</p>
+                </div>
+
+                <div class="col-4">
+                    <span class="font-m cl-gray-300 f-weight-700"><img src="assets/img/home/ajuda/icon-6.svg" alt="Icone Contabilidade Completa">Contabilidade Completa</span>
+                    <p class="cl-gray-300 font-s">Categorize as transações financeiras da sua empresa e fique preparado(a) para o seu próximo Imposto de Renda.</p>
+                </div>
+            </div>
+            <hr>
+        </div>
+    </div>
+</div>
+
+<div id="atendimento">
+    <div class="content">
+        <div class="row">
+            <div class="col-lg-5 col-md-12">
+                <img class="image1" src="assets/img/home/atendimento/image.svg">
+                <img class="image2" src="assets/img/home/atendimento/border.svg">
+            </div>
+            <div class="col-lg-7 col-md-12">
+                <div class="col-12 title-left" align="left">
+                    <h2 class="font-g cl-gray-300 f-weight-400"><b>Atendimento personalizado, pelo canal que preferir</b> </h2>
+                </div>
+                <div class="col-12">
+                    <p class="font-s">
+                        Nosso time de atendimento ao cliente está disponível de segunda a sexta-feira para auxiliar nas sua dúvidas, independente do seu plano de serviços.
+                    </p>
+                </div>
+
+                <div class="col-12">
+                    <div class="box bx-1 bg-gray-200 cl-gray-300 font-m">
+                        <img src="assets/img/home/atendimento/icon-1.svg">
+                        <span class="f-weight-700">Telefone</span>
+                        <p>3003 8384</p>
+                    </div>
+
+                    <div class="box bx-2 bg-gray-100 cl-gray-300 font-m">
+                        <img src="assets/img/home/atendimento/icon-2.svg">
+                        <span class="f-weight-700">Whatsapp</span>
+                        <p>3003 8384</p>
+                    </div>
+
+                    <div class="box bx-3 bg-gray-200 cl-gray-300 font-m">
+                        <img src="assets/img/home/atendimento/icon-3.svg">
+                        <span class="f-weight-700">E-mail</span>
+                        <p>contato@contabilix.com.br</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="como-funciona" class="bg-gray-100">
+    <div class="content">
+        <div class="row">
+            <div class="col-12 buttons">
+                <a href="javascript:;" class="bt active cl-white f-weight-700">Como abrir minha empresa?</a>
+                <a href="javascript:;" class="bt cl-white f-weight-700">Como trocar de contador?</a>
+            </div>
+            <div class="col-12">
+                <h3 class="font-xg cl-gray-300 f-weight-700">Como funciona a abertura</h3>
+                <p class="cl-gray-300 font-s">O processo de abertura é feito online, de forma fácil e gratuita, sendo cuidado por especialistas durante toda a duração.</p>
+            </div>
+            <div class="row">
+                <div class="col-3">
+                    <div class="box bx1 bg-white cl-gray-300">
+                        <span class="font-m"><span>01</span> Preencha seu cadastro</span>
+                        <p class="font-s">Informe alguns dados iniciais de sua empresa e realize o crédito de abertura (este voltará como desconto já na sua primeira mensalidade).
+                        </p>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="box bx2 bg-white cl-gray-300">
+                        <span class="font-m"><span>02</span> Cuidamos de toda a documentação</span>
+                        <p class="font-s">Nossos contadores irão validar seus dados e lhe sugerir o tipo de empresa que fará você pagar o mínimo de impostos quando começar a faturar. Você aprovando, lhe enviamos os documentos digitalmente.</p>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="box bx1 bg-white cl-gray-300">
+                        <span class="font-m"><span>03</span> Protocolo nos órgãos</span>
+                        <p class="font-s">A documentação deverá ser impressa, assinada e protocolada na Junta Comercial ou Cartório, caso não deseje sair de casa, basta nos devolver digitalmente junto com uma procuração que protocolamos por você.</p>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="box bx2 bg-white cl-gray-300">
+                        <span class="font-m"><span>04</span> Pronto! Sua empresa está aberta.</span>
+                        <p class="font-s">Com o CNPJ em mãos e a empresa 100% regular você está pronto para emitir notas fiscais e começar a faturar em sua nova empresa.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="comentarios" class="bg-blue-800">
+    <div class="bg-image">
+        <div class="content">
+            <div class="row">
+                <div class="col-12 title-left" align="left">
+                    <h2 class="font-g cl-white f-weight-400"><b>O que nossos clientes estão falando</b> </h2>
+                </div>
+                <div class="col-12">
+                    <img class="avaliation" src="assets/img/home/comentarios/rate1.svg">
+                    <img class="avaliation" src="assets/img/home/comentarios/rate2.svg">
+                </div>
+                <div class="col-12 boxes-comentarios ">
+                    <div class="box">
+                        <img class="detail1" src="assets/img/home/comentarios/aspas.svg">
+                        <p class="font-s">
+                            Com certeza é uma nova forma de lidar com a contabilidade da minha empresa. O que gostaria de ressaltar foi quando precisei tirar dúvidas, os contadores foram muito atenciosos e didáticos. Obrigado, Contabilix
+                        </p>
+                        <span>
+                            <img src="assets/img/home/comentarios/image.svg">
+                            <div class="font-s">
+                                <span class="cl-gray-300 italic f-weight-700">Jacob Serryua</span>
+                                <span class="cl-blue-300 f-weight-700">Controller Informática</span>
+                            </div>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="faq">
+    <div class="content">
+        <div class="row">
+            <div class="col-12 title" align="center">
+                <h2 class="font-g cl-gray-300 f-weight-400"><b>Perguntas e respostas</b> </h2>
+            </div>
+            <div class="col-lg-4 col-md-12 boz">
+                <img src="assets/img/home/faq/image.svg">
+                <span class="font-g cl-blue-700 f-weight-700">Ainda está com dúvidas?</span>
+                <p class="cl-gray-300 font-s">Acesse nossa Central de atendimento e tire suas dúvidas. Clique abaixo:</p>
+                <a href="javascript:;" class="bt bg-green-200 cl-white font-m">CENTRAL DE ATENDIMENTO</a>
+            </div>
+
+            <div class="col-lg-8 col-md-12">
+                <ul>
+                    <li>
+                        <a href="javascript:;" class="bg-gray-100 cl-gray-300 font-s">O que é a Contabilix? <i class="fa fa-sort-right icon cl-green-200"></i></a>
+                        <div class="text">
+                            <span class="font-s cl-blue-400">Somos um Escritório de Contabilidade Online.</span>
+                            <p class="cl-gray-300 font-s">Graças a automatização de todas as tarefas executadas nos escritórios tradicionais, conseguimos ter preços incríveis para a contabilidade da sua empresa.
+                                Disponibilizamos online suas Demonstrações Contábeis, tais como: Folha de Pagamentos, Impostos, Livro Caixa, Relatórios e outros, além de realizar todas as obrigações legais da sua empresa perante todas as esferas do governo.</p>
+                        </div>
+                    </li>
+
+                    <li>
+                        <a href="javascript:;" class="bg-gray-100 cl-gray-300 font-s">O que é a Contabilix? <i class="fa fa-sort-right icon cl-green-200"></i></a>
+                        <div class="text">
+                            <span class="font-s cl-blue-400"></span>
+                            <p class="cl-gray-300 font-s"></p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="blog">
+    <div class="content">
+        <div class="row">
+            <div class="col-12 title" align="center">
+                <h2 class="font-g cl-gray-300 f-weight-400"><b>Blog Contabilix</b> </h2>
+                <p class="cl-gray-300 font-s">Se atualize com conteúdos importantes para a sua empresa.</p>
+            </div>
+
+            <div class="col-lg-4 col-md-12">
+                <div class="box">
+<!--                    <img src="assets/img/home/blog/image.svg">-->
+                    <p class="font-s f-weight-700">Como definir o capital social da empresa em 4 passos simples</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-12">
+                <div class="box">
+<!--                    <img src="assets/img/home/blog/image.svg">-->
+                    <p class="font-s f-weight-700">Como definir o capital social da empresa em 4 passos simples</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-12">
+                <div class="box">
+<!--                    <img src="assets/img/home/blog/image.svg">-->
+                    <p class="font-s f-weight-700">Como definir o capital social da empresa em 4 passos simples</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div id="na-midia" class="bg-gray-100">
+    <div class="content">
+        <div class="col-2 title-left" align="left">
+            <h2 class="font-g cl-gray-300 f-weight-400"><b>Na mídia</b> </h2>
+        </div>
+
+        <div class="col-10">
+            <div class="box">
+                <img src="assets/img/home/midia/logo-2.svg">
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include "partials/footer.phtml" ?>
